9253d5959dcbaa6b82780c7f05a4a448ac80bb81
MUNIZZ <77640190+peodt13@users.noreply.github.com>
2024-05-05 00:24:25 -0300

    Add files via upload

--- tabela2.js
+++ tabela2.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     var botaoEnviar = document.querySelector('.add-encomenda');
     botaoEnviar.addEventListener('click', lidarComEnvioDoFormulario);
+
+    // Adiciona ouvinte de evento para remover linhas quando a página é carregada
+    adicionarOuvinteRemoverLinha();
 });
 
 function lidarComEnvioDoFormulario(evento) {
@@ -20,12 +23,15 @@ function lidarComEnvioDoFormulario(evento) {
     }
 }
 
-function validaQtde(qtde) {
-    return !isNaN(qtde) && parseFloat(qtde) > 0;
-}
-
-function validaValor(valor) {
-    return !isNaN(valor) && parseFloat(valor) > 0;
+function adicionarOuvinteRemoverLinha() {
+    document.querySelectorAll('.linha').forEach(function(linha) {
+        linha.addEventListener('dblclick', function() {
+            var confirmacao = confirm("Tem certeza que deseja apagar esta linha?");
+            if (confirmacao) {
+                linha.remove();
+            }
+        });
+    });
 }
 
 function adicionarEncomendaNaTabela(nome, produto, quantidade, valor_unitario) {
@@ -41,13 +47,35 @@ function adicionarEncomendaNaTabela(nome, produto, quantidade, valor_unitario) {
     var valorTotal = parseFloat(quantidade) * parseFloat(valor_unitario);
     var novaCelulaTotal = novaLinha.insertCell(valoresDasCelulas.length);
     novaCelulaTotal.textContent = formataValor(valorTotal);
+
+    // Adiciona ouvinte de evento de remover linha à linha recém-adicionada
+    adicionarOuvinteRemoverLinhaParaLinha(novaLinha);
 }
 
-function limparCamposDoFormulario() {
-    var formulario = document.querySelector('.container form');
-    formulario.reset();
+function adicionarOuvinteRemoverLinhaParaLinha(linha) {
+    linha.addEventListener('dblclick', function() {
+        var confirmacao = confirm("Tem certeza que deseja apagar esta linha?");
+        if (confirmacao) {
+            linha.remove();
+        }
+    });
+}
+
+function validaQtde(qtde) {
+    return !isNaN(qtde) && parseFloat(qtde) > 0;
+}
+
+function validaValor(valor) {
+    return !isNaN(valor) && parseFloat(valor) > 0;
 }
 
 function formataValor(valor) {
     return valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+}
+
+function limparCamposDoFormulario() {
+    var formulario = document.querySelector('.container form');
+    if (formulario) {
+        formulario.reset();
+    }
 }
